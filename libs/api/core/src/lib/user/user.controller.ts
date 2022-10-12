90f6331ccf0f63dde18b56afad5eac5ed5fe61bf
Quang HUYNH <htquangg@gmail.com>
2022-10-12 22:06:38 +0700

    feat: Get user info through **CurrentUser** decorator

--- libs/api/core/src/lib/user/user.controller.ts
+++ libs/api/core/src/lib/user/user.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get, Post } from '@nestjs/common';
+
 import { UserService } from './user.service';
 
+import {
+  AuthUserDto,
+  CurrentUser,
+  Roles,
+  USER_ROLES,
+} from '@everfit/api/common';
+import { User } from '@everfit/api/entities';
+
 @Controller('/user')
+@Roles(USER_ROLES.APP)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('/me')
-  getCurrentUser() {
-    // TODO: get current user information
+  async getCurrentUser(@CurrentUser() currentUser: AuthUserDto): Promise<User> {
+    return await this.userService.getUserInformation(currentUser);
   }
 
   @Get('/body-vitals')
--- libs/api/core/src/lib/user/user.service.ts
+++ libs/api/core/src/lib/user/user.service.ts
@@ -2,10 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { AuthUserDto } from '../auth';
 import { BodyVitalsService } from '../body-vitals';
 
-import { EverfitBaseService } from '@everfit/api/common';
+import { AuthUserDto, EverfitBaseService } from '@everfit/api/common';
 import { CachingService } from '@everfit/api/services';
 import { User } from '@everfit/api/entities';
 import { randomStringGenerator } from '@everfit/shared/utils';
@@ -60,16 +59,16 @@ export class UserService extends EverfitBaseService<User> {
     );
   }
 
+  async getUserInformation(currentUser: AuthUserDto): Promise<User> {
+    return await this.getUserById(currentUser.userId);
+  }
+
   async createUser(params: Partial<User>) {
     const user = {
       ...params,
       id: randomStringGenerator(),
     };
     return (await this.save(this.create(user))) as User;
-  }
-
-  async getUserInformation(currentUser: AuthUserDto): Promise<User> {
-    return await this.getUserById(currentUser.userId);
   }
 
   async getBodyVitalsLog() {}
