b78cd8094cd6a2807c206e881c9243a1237d277b
Quang HUYNH <htquangg@gmail.com>
2022-10-16 15:20:44 +0700

    feat(libs/shared): apply-mixins util

--- libs/shared/utils/src/lib/index.ts
+++ libs/shared/utils/src/lib/index.ts
@@ -2,3 +2,4 @@ export * from './create-uid.util';
 export * from './random-string-generator.util';
 export * from './check.util';
 export * as is from './is.util';
+export * from './apply-mixins.util';
